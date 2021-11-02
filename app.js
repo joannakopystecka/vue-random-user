ada6593f73e107a009daa1985fb66862af8cc1c2
joannakopystecka <asia.kopystecka@gmail.com>
2021-11-02 22:23:55 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,27 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      firstName: "Joanna",
+      lastName: "Kopystecka",
+      email: "asia@oo.pl",
+      gender: "male",
+      picture: "https://randomuser.me/api/portraits/men/75.jpg",
+    };
+  },
+
+  methods: {
+    async getUser() {
+      const res = await fetch("https://randomuser.me/api");
+      const { results } = await res.json();
+      console.log(results);
+
+      this.firstName = results[0].name.first;
+      this.lastName = results[0].name.last;
+      this.email = results[0].email;
+      this.gender = results[0].gender;
+      this.picture = results[0].picture.large;
+    },
+  },
+});
+
+app.mount("#app");
